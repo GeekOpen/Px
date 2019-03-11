4ae1b40311451fe6b24e215d28cd1ca90f7dc5f4
Geek Open <angeslikak@gmail.com>
2019-03-11 04:23:15 +0100

    Init project

---
+++ app/src/main/java/xapp/c2i/tech/px/SpashOpen.java
@@ -0,0 +1,13 @@
+package xapp.c2i.tech.px;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpashOpen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_open);
+    }
+}
